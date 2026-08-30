78a61272958a79a955d526ea7b9c30bea4e444cf
sainiaman3478-source <sainiaman3478@gmail.com>
2026-08-30 11:19:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Kisansathi-ai/",
+  base: process.env.GITHUB_ACTIONS ? "/Kisansathi-ai/" : "/",
   plugins: [react()],
   server: {
     proxy: {
